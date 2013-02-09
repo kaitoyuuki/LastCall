69f56c2b851c4291aaee0b8efe82790a04b9b88a
Ian Finley <ianfinleyhome@gmail.com>
2013-02-08 20:39:25 -0500

    split /play into a seperate class file; started working on methods to get item IDs from disc names and disc names from material names; changed /play so that if a player uses /play while holding a disc, it will play that disc for them.

--- src/com/github/kaitoyuuki/LastCall/LCMain.java
+++ src/com/github/kaitoyuuki/LastCall/LCMain.java
@@ -8,7 +8,7 @@ public final class LCMain extends JavaPlugin {
 	public void onEnable() {
 		this.saveDefaultConfig();
 		getConfig();
-		getCommand("play").setExecutor(new LastCallCommands(this));
+		getCommand("play").setExecutor(new LastCallPlay(this));
 		getCommand("lastcall").setExecutor(new LastCallCommands(this));
 		getCommand("lc").setExecutor(new LastCallCommands(this));
 	}
---
+++ src/com/github/kaitoyuuki/LastCall/LastDiscs.java
@@ -0,0 +1,53 @@
+package com.github.kaitoyuuki.LastCall;
+
+import org.bukkit.Material;
+//import org.bukkit.inventory.ItemStack;
+
+public class LastDiscs {
+	public String getDiscName(Material disc){
+		String discName = null;
+		
+		return discName;
+	}
+	
+	public int getDiscID(final String LastSong) {
+		int discID = 0;
+		if (LastSong == "13") {
+			discID =  2256;
+		}
+		if (LastSong.equalsIgnoreCase("cat")) {
+			discID =  2257;
+		}
+		if (LastSong.equalsIgnoreCase("blocks")) {
+			discID =  2258;
+		}
+		if (LastSong.equalsIgnoreCase("chirp")) {
+			discID =  2259;
+		}
+		if (LastSong.equalsIgnoreCase("far")) {
+			discID =  2260;
+		}
+		if (LastSong.equalsIgnoreCase("mall")) {
+			discID =  2261;
+		}
+		if (LastSong.equalsIgnoreCase("mellohi")) {
+			discID =  2262;
+		}
+		if (LastSong.equalsIgnoreCase("stal")) {
+			discID =  2263;
+		}
+		if (LastSong.equalsIgnoreCase("strad")) {
+			discID =  2264;
+		}
+		if (LastSong.equalsIgnoreCase("ward")) {
+			discID =  2265;
+		}
+		if (LastSong == "11") {
+			discID =  2266;
+		}
+		if (LastSong.equalsIgnoreCase("wait")) {
+			discID =  2267;
+		}
+		return discID;
+	}
+}

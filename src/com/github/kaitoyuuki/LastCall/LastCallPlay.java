efd63cfe58e8640afc53e768ef604c4a70561cc3
Ian Finley <ianfinleyhome@gmail.com>
2013-02-11 14:19:37 -0500

    made the rest of /play code use the getDiscID method

--- src/com/github/kaitoyuuki/LastCall/LastCallPlay.java
+++ src/com/github/kaitoyuuki/LastCall/LastCallPlay.java
@@ -8,7 +8,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-//import com.github.kaitoyuuki.LastCall.LastDiscs;
+
 
 public class LastCallPlay extends com.github.kaitoyuuki.LastCall.LastDiscs implements CommandExecutor {
 
@@ -37,8 +37,6 @@ public class LastCallPlay extends com.github.kaitoyuuki.LastCall.LastDiscs imple
 				}
 				if (args.length == 1) {
 					int discID = 0;
-					//String disc = args[0];
-					// cat 2257
 					String PlaySong = args[0];
 					discID = disc.getDiscID(PlaySong);
 					if (discID != 0) { 
@@ -51,7 +49,6 @@ public class LastCallPlay extends com.github.kaitoyuuki.LastCall.LastDiscs imple
 
 						return true;
 					}
-					// not a valid disc
 					else {
 						sender.sendMessage("Not a valid disc!");
 						return false;
@@ -64,106 +61,19 @@ public class LastCallPlay extends com.github.kaitoyuuki.LastCall.LastDiscs imple
 						return false;
 					}
 					else {
-						//String disc = args[1];
-						// cat 2257
-						if (args[1].equalsIgnoreCase("cat")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2257);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// 13 2256
-						if (args[1].equals("13")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2256);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// blocks 2258
-						if (args[1].equalsIgnoreCase("blocks")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2258);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// chirp 2259
-						if (args[1].equalsIgnoreCase("chirp")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2259);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// far 2260
-						if (args[1].equalsIgnoreCase("far")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2260);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// mall 2261
-						if (args[1].equalsIgnoreCase("mall")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2261);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// mellohi 2262
-						if (args[1].equalsIgnoreCase("mellohi")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2262);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}							
-						// stal 2263
-						if (args[1].equalsIgnoreCase("stal")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2263);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// strad 2264
-						if (args[1].equalsIgnoreCase("strad")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2264);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// ward 2265
-						if (args[1].equalsIgnoreCase("ward")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2265);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// 11 2266
-						if (args[1].equals("11")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2267);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						// wait 2267
-						if (args[1].equalsIgnoreCase("wait")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2268);
-							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-							return true;
-						}
-						else {
+						int discID = 0;
+						String PlaySong = args[1];
+						discID = disc.getDiscID(PlaySong);
+						if (discID == 0) {
 							sender.sendMessage("Not a valid disc!");
 							return false;
+						}
+						else {
+							Location loc = target.getLocation();
+							Effect effect = Effect.RECORD_PLAY;
+							target.playEffect(loc, effect, discID);
+							sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
+							return true;
 						}
 					}
 				}
@@ -188,107 +98,19 @@ public class LastCallPlay extends com.github.kaitoyuuki.LastCall.LastDiscs imple
 					}
 					if (args.length == 1) {
 						Player target = (Player) sender;
-						//String disc = args[1];
-						// cat 2257
-						if (args[0].equalsIgnoreCase("cat")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2257);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// 13 2256
-						if (args[0].equals("13")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2256);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// blocks 2258
-						if (args[0].equalsIgnoreCase("blocks")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2258);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// chirp 2259
-						if (args[0].equalsIgnoreCase("chirp")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2259);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// far 2260
-						if (args[0].equalsIgnoreCase("far")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2260);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// mall 2261
-						if (args[0].equalsIgnoreCase("mall")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2261);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// mellohi 2262
-						if (args[0].equalsIgnoreCase("mellohi")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2262);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}							
-						// stal 2263
-						if (args[0].equalsIgnoreCase("stal")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2263);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// strad 2264
-						if (args[0].equalsIgnoreCase("strad")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2264);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// ward 2265
-						if (args[0].equalsIgnoreCase("ward")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2265);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// 11 2266
-						if (args[0].equals("11")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2267);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// wait 2267
-						if (args[0].equalsIgnoreCase("wait")) {
-							Location loc = target.getLocation();
-							Effect effect = Effect.RECORD_PLAY;
-							target.playEffect(loc, effect, 2268);
-							sender.sendMessage("Now playing " + args[0]);
-							return true;
-						}
-						// not a valid disc
-						else {
+						int discID = 0;
+						String PlaySong = args[0];
+						discID = disc.getDiscID(PlaySong);
+						if (discID == 0) {
 							sender.sendMessage("Not a valid disc!");
 							return false;
+						}
+						else {
+							Location loc = target.getLocation();
+							Effect effect = Effect.RECORD_PLAY;
+							target.playEffect(loc, effect, discID);
+							sender.sendMessage("Now playing " + args[0]);
+							return true;
 						}
 					}
 					if (args.length == 2) {
@@ -299,106 +121,19 @@ public class LastCallPlay extends com.github.kaitoyuuki.LastCall.LastDiscs imple
 						}
 						else {
 							if (target != player && player.hasPermission("lastcall.play.others") || target == player) {
-								//String disc = args[1];
-								// cat 2257
-								if (args[1].equalsIgnoreCase("cat")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2257);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// 13 2256
-								if (args[1].equals("13")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2256);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// blocks 2258
-								if (args[1].equalsIgnoreCase("blocks")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2258);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// chirp 2259
-								if (args[1].equalsIgnoreCase("chirp")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2259);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// far 2260
-								if (args[1].equalsIgnoreCase("far")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2260);
-									sender.sendMessage("Now playing" + args[1] + " for " + args[0]);
-									return true;
-								}
-								// mall 2261
-								if (args[1].equalsIgnoreCase("mall")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2261);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// mellohi 2262
-								if (args[1].equalsIgnoreCase("mellohi")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2262);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}							
-								// stal 2263
-								if (args[1].equalsIgnoreCase("stal")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2263);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// strad 2264
-								if (args[1].equalsIgnoreCase("strad")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2264);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// ward 2265
-								if (args[1].equalsIgnoreCase("ward")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2265);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// 11 2266
-								if (args[1].equals("11")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2267);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								// wait 2267
-								if (args[1].equalsIgnoreCase("wait")) {
-									Location loc = target.getLocation();
-									Effect effect = Effect.RECORD_PLAY;
-									target.playEffect(loc, effect, 2268);
-									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
-									return true;
-								}
-								else {
+								int discID = 0;
+								String PlaySong = args[1];
+								discID = disc.getDiscID(PlaySong);
+								if (discID == 0) {
 									sender.sendMessage("Not a valid disc!");
 									return false;
+								}
+								else {
+									Location loc = target.getLocation();
+									Effect effect = Effect.RECORD_PLAY;
+									target.playEffect(loc, effect, discID);
+									sender.sendMessage("Now playing " + args[1] + " for " + args[0]);
+									return true;
 								}
 							}
 						}
